4d63d7d1739a38f18a54116ce73ef815877890a8
Mustafif <mustafif0929@gmail.com>
2022-01-03 22:07:15 -0500

    Update

--- texcreate_lib/src/Web/web.rs
+++ texcreate_lib/src/Web/web.rs
@@ -3,10 +3,16 @@ use rocket::{Response, Request, get, routes};
 use rocket::fs::{FileServer, NamedFile, relative};
 use std::path::{Path, PathBuf};
 use rocket::{Rocket, Build};
-use crate::Web::tex_create;
+use crate::Web::{tex_create, tc_html};
+
 
 #[get("/")]
 pub async fn index() -> Option<NamedFile>{
+    let index = Path::new("index.html");
+    if !index.exists(){
+        std::fs::File::create(&index).unwrap();
+        std::fs::write(&index, tc_html.as_bytes()).unwrap();
+    }
     NamedFile::open("index.html").await.ok()
 }
 
